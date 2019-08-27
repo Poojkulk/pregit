8040e3e13e67fa65a7271859cb2547a2ddaf631c
pooja <pooja@gmail.com>
2019-08-27 11:17:01 +0530

    javaScript file are added

---
+++ index.js
@@ -0,0 +1,5 @@
+ // Method returns true if a vowel exists in the input string. Returns false otherwise.
+String.prototype.hasVowels = function hasVowels() {
+  const vowels = new RegExp('[aeiou]', 'i');
+  return vowels.test(this);
+};
